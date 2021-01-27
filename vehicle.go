9fd5b9d4d8da5a4ce3f0c41ef571b5ea85fe0e89
xcy0 <xuchy@hotmail.com>
2021-01-27 23:07:39 +0000

    module3

--- vehicle.go
+++ vehicle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -26,6 +27,19 @@ type truck struct {
 type bike struct {
 	model string
 	make  string
+}
+
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car", c.make, c.model)
+	showRating(c.model)
+}
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
+	showRating(b.model)
+}
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
+	showRating(t.model)
 }
 
 // Values array for the feedback.json file
@@ -82,7 +96,33 @@ func main() {
 
 	// Generate ratings for the different vehicles
 	generateRating()
+
 	// Print ratings for the different vehicles
+	for _, veh := range inventory {
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+	}
+}
+
+func showRating(model string) {
+	ratingFound := false
+	for m, r := range vehicleResult {
+		if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
+		}
+	}
+	if !ratingFound {
+		fmt.Printf("No rating for this vehicle")
+	}
 }
 
 func readJSONFile() Values {
